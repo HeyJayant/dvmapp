f1390ae479455c91845df696dbff85b05eb10f82
Jayant Yadav <jayantestudy@gmail.com>
2024-03-08 00:09:09 +0530

    Delete lib directory

--- lib/content/fire.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:dvmapp/content/content.dart';
-
-class FireContent extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return PokemonContentList(type: '10');
-  }
-}
